0ab5d4b78d5cee5a9e62d319aed486408c99e0f5
itsaFan <steffansim@gmail.com>
2023-12-06 11:01:04 +0800

    app.listen

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const config = require("./config/config");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
 
-app.listen(8080, () => console.log("Server is Running"));
+app.listen(config.port, () => console.log(`Server is running on http://localhost:${config.port}`));
