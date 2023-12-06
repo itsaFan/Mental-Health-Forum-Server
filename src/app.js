55cf63427cee30e625004fbe7afab13127ca5250
itsaFan <steffansim@gmail.com>
2023-12-06 21:23:16 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const dbConnection = require("./config/db-config");
 
 const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 const app = express();
 app.use(cookieParser());
@@ -14,5 +15,6 @@ dbConnection();
 
 // Routing
 app.use("/api", authRoutes);
+app.use("/api/profile", profileRoutes);
 
 app.listen(config.port, () => console.log(`Server is running on http://localhost:${config.port}`));
--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const userProfileController = require("../controllers/userProfileController");
 
+const router = express.Router()
+
 router.patch("/user-profile/:userId", userProfileController.updateProfile);
 
 module.exports = router;
