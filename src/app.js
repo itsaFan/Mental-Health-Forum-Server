22a4383514edc3b2e86be3b4b98a5d4ee7caedef
itsaFan <122992976+itsaFan@users.noreply.github.com>
2023-12-06 11:30:43 +0800

    feat/db: create db connection & seeding (#2)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const config = require("./config/config");
 const cookieParser = require("cookie-parser");
+const dbConnection = require("./config/db-config");
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+
+// DB Connection
+dbConnection;
 
 app.listen(config.port, () => console.log(`Server is running on http://localhost:${config.port}`));
