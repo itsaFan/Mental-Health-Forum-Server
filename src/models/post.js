e3ef40e3257974f122200814e7c7d967ae059f64
itsaFan <steffansim@gmail.com>
2023-12-06 20:31:02 +0800

    add post schema

---
+++ src/models/post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const postSchema = new mongoose.Schema({
+  content: {
+    type: String,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [commentSchema],
+});
