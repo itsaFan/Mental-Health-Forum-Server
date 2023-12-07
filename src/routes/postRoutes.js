eeed774d5731a190538cfe4cc1e3e091809272a3
itsaFan <steffansim@gmail.com>
2023-12-07 22:02:15 +0800

    restrict endpoint with role

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { verifyAccessToken } = require("../middlewares/verifyJwt");
 const { createPost, editPost } = require("../controllers/postController");
+const { checkRole } = require("../middlewares/roleAuth");
 
 const router = express.Router();
 
-router.post("/", verifyAccessToken, createPost);
-router.patch("/edit/:postId", verifyAccessToken, editPost);
+router.post("/", verifyAccessToken, checkRole(["ROLE_USER", "ROLE_MODERATOR"]), createPost);
+router.patch("/edit/:postId", verifyAccessToken, checkRole(["ROLE_USER", "ROLE_MODERATOR"]), editPost);
 
 module.exports = router;
