8232d3b48edb2b8f65eeea22497440bf25fef92c
itsaFan <steffansim@gmail.com>
2023-12-07 21:55:16 +0800

    fix import

--- src/middlewares/corsConfig.js
+++ src/middlewares/corsConfig.js
@@ -1,13 +1,12 @@
 const config = require("../config/config");
-
+const cors = require("cors");
 
 const corsOptions = {
-    origin: [config.devUrl, config.mainUrl],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 204,
+  origin: [config.devUrl, config.mainUrl],
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  credentials: true,
+  optionsSuccessStatus: 204,
 };
-
 
 const corsMiddleware = cors(corsOptions);
 
