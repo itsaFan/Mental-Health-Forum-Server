2c53bda73e2f8a175814b4a7d1e4b8163a4220ef
itsaFan <122992976+itsaFan@users.noreply.github.com>
2023-12-07 21:52:10 +0800

    createPost and editPost logic done (#12)

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const { verifyAccessToken } = require("../middlewares/verifyJwt");
+const { createPost, editPost } = require("../controllers/postController");
 
+const router = express.Router();
+
+router.post("/", verifyAccessToken, createPost);
+router.patch("/edit/:postId", verifyAccessToken, editPost);
+
+module.exports = router;
