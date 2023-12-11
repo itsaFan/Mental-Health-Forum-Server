f7e184f6273c2f46779db8e96a1588344da814a2
itsaFan <steffansim@gmail.com>
2023-12-11 12:47:30 +0800

    add role restriction

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.post("/:postId", verifyAccessToken, checkRole(["ROLE_USER", "ROLE_MODERATOR", "ROLE_ADMIN"]), commentToPost);
 router.patch("/:postId", verifyAccessToken, checkRole(["ROLE_USER", "ROLE_MODERATOR", "ROLE_ADMIN"]), editOwnComment);
-router.delete("/:postId", verifyAccessToken, deleteOwnComment);
+router.delete("/:postId", verifyAccessToken, checkRole(["ROLE_USER", "ROLE_MODERATOR", "ROLE_ADMIN"]), deleteOwnComment);
 
 module.exports = router;
