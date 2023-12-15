50c9981b7995ed1ec1841339616f9c58a74af4fa
itsaFan <steffansim@gmail.com>
2023-12-16 00:11:23 +0800

    change res msg

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,7 +63,7 @@ const login = async (req, res) => {
     }
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "Username or Email not found" });
     }
 
     if (!user.verifyPassword(password)) {
